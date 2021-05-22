559b694fd996a537c53a8c711d6792b4abad74ea
FAKE <83130495+INX-PRO@users.noreply.github.com>
2021-05-22 19:32:01 +0300

    почити когнец

--- прикольное имя.py
+++ прикольное имя.py
@@ -2,14 +2,12 @@ from pygame import *
 from random import*
 #font.init()
 
-r = Player("r.png",299,435,5)
-rr = Player("rr.png",299,435,5)
-rrr = Player("rrr.png",299,435,5)
+
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (65, 65))
+        self.image = transform.scale(image.load(player_image),(wight, height))
         self.speed = player_speed
 
         self.rect = self.image.get_rect()
@@ -21,6 +19,8 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y)) 
 
 
+
+
 class Player(GameSprite):
     def update_l(self):
         keys = key.get_pressed()
@@ -30,7 +30,9 @@ class Player(GameSprite):
 
         if keys[K_s] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
+
     def update_r(self):
+        keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
 
@@ -38,10 +40,15 @@ class Player(GameSprite):
             self.rect.y += self.speed
 
 
+r = Player("r2.1.png",9,5,5,75,75)
+rr = Player("rr.png",5,250,5,50,150)
+rrr = Player("rrr.png",546,250,5,50,150)
+
+
 back = (200,255,255)
 win_width = 600
-win_hight = 500
-window =  display.set_mode((win_width,win_hight))
+win_height = 500
+window =  display.set_mode((win_width,win_height))
 window.fill(back)
 #display.set_caption("пинпонг")
 #background = transform.scale(image.load('galaxy.jpg'),(700,500))
@@ -51,15 +58,48 @@ clock = time.Clock()
 FPS = 120
 game = True
 
+speed_x = 3
+speed_y = 3
 
+game = True
+Finish = False
+font = font.SysFont(None,50)
+win = font.render('YOU WIN',True,(255,215,0))
+los = font.render('YOU LOSE',True,(255,0,0))
 
 while game:
     
     for e in event.get():
         if e.type == QUIT:
-            game = False        
+            game = False 
+
+    window.fill(back)
+    r.rect.x += speed_x
+    r.rect.y += speed_y
+
+    if r.rect.y > 470 or  r.rect.y < 0:
+        speed_y *= -1      
 
 
+
+    if sprite.collide_rect(rr, r) or sprite.collide_rect(rrr, r):
+        speed_x *= -1
+        #score += 1
+
+    """if Finish != True:
+        
+        if ball.rect.x < 0:
+            window.blit(win2,(290,230))
+
+        elif ball.rect.x > 700:
+            window.blit"""
+
+    r.update()
+    r.reset()
+    rr.update_l()
+    rr.reset()
+    rrr.update_r()
+    rrr.reset()
 
     clock.tick(FPS)
     display.update()
